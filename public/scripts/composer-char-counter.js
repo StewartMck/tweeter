5c8e08bffcb34beeb77fb6c3b69cb34565998768
Stewart McKinlay <stewart.mckinlay@gmail.com>
2020-12-15 18:39:08 +0000

    refactoring

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
   const counterDefaultColor = $('.container').find('.counter').css('color');
   $('.container').find('.tweet-text').keyup(function(event) {
-    let numberChars = event.target.value.length;
-    let charsLeft = 140 - numberChars;
-    let counterColor = charsLeft < 0 ? 'red' : counterDefaultColor;
+    const numberChars = event.target.value.length;
+    const charsLeft = 140 - numberChars;
+    const counterColor = charsLeft < 0 ? 'red' : counterDefaultColor;
     $('.counter').html(charsLeft);
     $('.counter').css('color', counterColor);
   });
